a1f54da7b35490eec653279a45ff7c4ef710c6cf
Federico Ceratto <federico.ceratto@gmail.com>
2019-08-18 13:05:37 +0100

    Switch to Path, add type hints, capture stderr
    Handle existing L1

--- conbuilder.py
+++ conbuilder.py
@@ -3,9 +3,10 @@
 # Copyright 2017 Federico Ceratto <federico@debian.org>
 # Released under GPLv3 License, see LICENSE file
 
+from pathlib import Path
 import hashlib
 import os
-import os.path
+import sys
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from configparser import ConfigParser
@@ -90,13 +91,13 @@ Default configuration:
 )
 
 
-def run(cmd, quietcmd=False, quiet=False):
+def run(cmd: str, quietcmd=False, quiet=False) -> list:
     """Run command, capture output
     """
     assert isinstance(cmd, str), repr(cmd)
     if not quietcmd:
         print(cmd)
-    proc = Popen(cmd, shell=True, stdout=PIPE)
+    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
     out = []
     while True:
         line = proc.stdout.readline()
@@ -109,24 +110,27 @@ def run(cmd, quietcmd=False, quiet=False):
 
     proc.wait()
     if proc.returncode != 0:
+        print("-- Error --")
+        err = proc.stderr.read().decode()
+        print(err)
+        print("-----------")
         raise Exception("'{}' returned {}".format(cmd, proc.returncode))
 
     return out
 
 
-def mount(lower, upper, work, mnt):
+def mount(lower, upper, work, mnt: Path):
     cmd = "sudo mount -t overlay overlay " "-olowerdir={},upperdir={},workdir={} {}"
     cmd = cmd.format(lower, upper, work, mnt)
     run(cmd)
-    assert os.path.isfile(os.path.join(mnt, "usr/bin/apt"))
+    assert (mnt / "usr/bin/apt").is_file()
 
 
 def umount(path):
     run("sudo umount {}".format(path))
 
 
-def nspawn(rcmd, quiet=False, drop_capability="", system_call_filter=""):
-    assert isinstance(rcmd, str)
+def nspawn(rcmd: str, quiet=False, drop_capability="", system_call_filter="") -> list:
     cmd = "sudo systemd-nspawn -M conbuilder --chdir=/srv "
     if drop_capability:
         cmd += "--drop-capability={} ".format(drop_capability)
@@ -136,7 +140,7 @@ def nspawn(rcmd, quiet=False, drop_capability="", system_call_filter=""):
     return run(cmd, quiet=quiet)
 
 
-def _parse_build_deps(out):
+def _parse_build_deps(out: list) -> list:
     """Parse build deps from apt-get build-dep
     """
     deps = set()
@@ -150,8 +154,7 @@ def _parse_build_deps(out):
         assert version, "Cannot parse version from %r" % line
         deps.add((pkgname, version))
 
-    deps = sorted(deps)
-    return deps
+    return sorted(deps)
 
 
 def extract_build_dependencies(l1dir):
@@ -172,8 +175,8 @@ def extract_build_dependencies(l1dir):
 
 
 def load_conf_and_parse_args():
-    confighome = os.path.expanduser(os.environ.get("XDG_CONFIG_HOME", "~/.config"))
-    default_conf_fn = os.path.join(confighome, "conbuilder.conf")
+    confighome = Path(os.environ.get("XDG_CONFIG_HOME", "~/.config")).expanduser()
+    default_conf_fn = confighome / "conbuilder.conf"
 
     ap = ArgumentParser(epilog=help_msg, formatter_class=RawDescriptionHelpFormatter)
     ap.add_argument(
@@ -210,7 +213,7 @@ def load_conf_and_parse_args():
     cp = ConfigParser()
     with open(args.conf) as f:
         cp.read_file(f)
-    args.cachedir = cp["DEFAULT"]["cachedir"]
+    args.cachedir = Path(cp["DEFAULT"]["cachedir"])
     assert args.cachedir not in ("", "/"), "Invalid cache dir"
     args.export_dir = cp["DEFAULT"]["export_dir"]
 
@@ -220,21 +223,22 @@ def load_conf_and_parse_args():
     return args
 
 
-def create_l1(conf, l1dir):
+def create_l1(conf, l1dir: Path):
     """Run debootstrap to create the L1 FS
     """
+    if l1dir.exists():
+        print("Error: the base filesystem (L1) already exists")
+        sys.exit(1)
     print("Creating", l1dir)
-    os.makedirs(l1dir)
+    l1dir.mkdir(parents=True)
     cmd = (
-        "sudo debootstrap --include=apt --keyring=/etc/apt/trusted.gpg"
-        " --force-check-gpg {} {} http://httpredir.debian.org/debian"
+        "sudo debootstrap --include=apt "
+        " --force-check-gpg {} {} http://deb.debian.org/debian"
     )
     cmd = cmd.format(conf.codename, l1dir)
     run(cmd)
-    assert os.path.isfile(os.path.join(l1dir, "usr/bin/apt"))
-    assert os.path.isdir(os.path.join(l1dir, "etc")), "/etc not found in {} ".format(
-        l1dir
-    )
+    assert (l1dir / "usr/bin/apt").is_file()
+    assert (l1dir / "etc").is_dir(), "/etc not found in {} ".format(l1dir)
 
 
 def update_l1(conf, l1dir):
@@ -270,7 +274,7 @@ def create_l2(conf, l1dir, l2dir, l2workdir, l2mountdir, expected_deps):
         out = nspawn(cmd, quiet=(conf.verbose == 0))
         _parse_build_deps(out)
         cmd = "-D {} --overlay=$(pwd)::/srv  -- /usr/bin/apt-get clean"
-        with open(os.path.join(l2mountdir, ".deps.conbuilder"), "w") as f:
+        with (l2mountdir / ".deps.conbuilder").open("w") as f:
             f.write("\n".join(deps_list))
 
     finally:
@@ -283,17 +287,17 @@ def build(conf):
     success = False
 
     # L1: base system
-    l1dir = os.path.join(conf.cachedir, "l1", conf.codename)
-    if not os.path.exists(l1dir):
+    l1dir = conf.cachedir / "l1" / conf.codename
+    if not l1dir.exists():
         create_l1(conf, l1dir)
 
     # L2: build dependencies
 
     deps, dep_hash = extract_build_dependencies(l1dir)
 
-    l2dir = os.path.join(conf.cachedir, "l2", "fs", dep_hash)
-    l2workdir = os.path.join(conf.cachedir, "l2", "overlay_work", dep_hash)
-    l2mountdir = os.path.join(conf.cachedir, "l2", "overlay_mount", dep_hash)
+    l2dir = conf.cachedir / "l2" / "fs" / dep_hash
+    l2workdir = conf.cachedir / "l2" / "overlay_work" / dep_hash
+    l2mountdir = conf.cachedir / "l2" / "overlay_mount" / dep_hash
     print("[L1] Ready")
 
     if not os.path.exists(l2dir):
@@ -303,9 +307,9 @@ def build(conf):
         mount(l1dir, l2dir, l2workdir, l2mountdir)
         print("[L2] Ready")
 
-        l3dir = os.path.join(conf.cachedir, "l3", "fs", dep_hash)
-        l3workdir = os.path.join(conf.cachedir, "l3", "overlay_work", dep_hash)
-        l3mountdir = os.path.join(conf.cachedir, "l3", "overlay_mount", dep_hash)
+        l3dir = conf.cachedir / "l3" / "fs" / dep_hash
+        l3workdir = conf.cachedir / "l3" / "overlay_work" / dep_hash
+        l3mountdir = conf.cachedir / "l3" / "overlay_mount" / dep_hash
         if not os.path.exists(l3dir):
             print("[L3] Creating", l3dir)
             os.makedirs(l3dir)
@@ -313,7 +317,7 @@ def build(conf):
             os.makedirs(l3mountdir)
         try:
             mount(l2mountdir, l3dir, l3workdir, l3mountdir)
-            run("sudo cp -a . {}".format(os.path.join(l3mountdir, "srv")))
+            run("sudo cp -a . {}".format(l3mountdir / "srv"))
             # TODO: configurable --private-network
             cmd = "--private-network -D {} -- /usr/bin/dpkg-buildpackage {}"
             cmd = cmd.format(l3mountdir, " ".join(conf.extra_args))
@@ -350,21 +354,20 @@ def install(conf):
     """Create temporary layer, install package and deps
     """
     # L1: base system
-    l1dir = os.path.join(conf.cachedir, "l1", conf.codename)
-    if not os.path.exists(l1dir):
+    l1dir = conf.cachedir / "l1" / conf.codename
+    if not l1dir.exists():
         create_l1(conf, l1dir)
 
     # L2t: install
     dep_hash = "foo"
-    l2dir = os.path.join(conf.cachedir, "l2i", "fs", dep_hash)
-    l2workdir = os.path.join(conf.cachedir, "l2i", "overlay_work", dep_hash)
-    l2mountdir = os.path.join(conf.cachedir, "l2i", "overlay_mount", dep_hash)
+    l2dir = conf.cachedir / "l2i" / "fs" / dep_hash
+    l2workdir = conf.cachedir / "l2i" / "overlay_work" / dep_hash
+    l2mountdir = conf.cachedir / "l2i" / "overlay_mount" / dep_hash
 
     print("[L2i] Creating", l2dir)
-    if not os.path.exists(l2dir):
-        os.makedirs(l2dir)
-        os.makedirs(l2workdir)
-        os.makedirs(l2mountdir)
+    for d in (l2dir, l2workdir, l2mountdir):
+        d.mkdir(parents=True, exist_ok=True)
+
     try:
         mount(l1dir, l2dir, l2workdir, l2mountdir)
         print("[L2] Ready")
@@ -392,14 +395,17 @@ def show(conf):
     print("Layers:")
     for nick, path in (("L1", "l1"), ("L2", "l2/fs"), ("L3", "l3/fs")):
         print("  {}:".format(nick))
-        for item in os.scandir(os.path.join(conf.cachedir, path)):
-            size = run("sudo du -hs {}".format(item.path), quietcmd=True, quiet=True)
+        d = conf.cachedir / path
+        if not d.exists():
+            continue
+        for item in d.iterdir():
+            size = run("sudo du -hs {}".format(item), quietcmd=True, quiet=True)
             size = size[0].split("\t")[0]
             print("    {:35} {}".format(item.name, size))
-            deps_fn = os.path.join(item.path, ".deps.conbuilder")
-            if not os.path.isfile(deps_fn):
+            deps_fn = item / ".deps.conbuilder"
+            if not deps_fn.is_file():
                 continue
-            with open(deps_fn) as f:
+            with deps_fn.open() as f:
                 for line in f:
                     print("     ", line.rstrip())
             # TODO add age
@@ -411,11 +417,11 @@ def main():
     conf = load_conf_and_parse_args()
 
     if conf.action == "create":
-        l1dir = os.path.join(conf.cachedir, "l1", conf.codename)
+        l1dir = conf.cachedir / "l1" / conf.codename
         create_l1(conf, l1dir)
 
     elif conf.action == "update":
-        l1dir = os.path.join(conf.cachedir, "l1", conf.codename)
+        l1dir = conf.cachedir / "l1" / conf.codename
         update_l1(conf, l1dir)
 
     if conf.action == "build":
